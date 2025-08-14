25c6e84d0810427e8e46fa6369ef05fb3b982522
Yash <86472828+xsupremeyx@users.noreply.github.com>
2025-08-14 10:46:47 +0000

    Add basic calculator html, styles and operate fn
    -> Added basic html structure for calculator
    -> Added basic styles to calculator body
    -> implemented basic add,sub,mul,div fns
    -> implemented operate fn to handle operations

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('hello');
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+function operate(opr1,opr2,operator){
+    if(operator === "+") return add(opr1,opr2);
+    else if(operator === "-") return sub(opr1,opr2);
+    else if(operator === "*") return multiply(opr1,opr2);
+    else if(operator === "/") return divide(opr1,opr2);
+    else return NaN;
+}
